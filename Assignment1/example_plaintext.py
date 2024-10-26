4705dfa23f85bef178c35f5094d708a875f1fe0f
Davidpereira2803 <david.pereira.002@student.uni.lu>
2024-10-26 14:12:55 +0200

    upd

---
+++ Assignment1/example_plaintext.py
@@ -0,0 +1,13 @@
+# %% [markdown]
+# # Plaintext Assignment
+#
+# Build this with `make build/example_plaintext.html`.
+#
+# Everything works as usual, including $\LaTeX.$
+
+
+# %%
+x = 42
+x * 2
+
+# %%
